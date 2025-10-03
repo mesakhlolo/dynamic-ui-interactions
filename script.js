227831e54d45f9f403172283ef2badf272453284
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-10-03 17:27:10 +0700

    cara simple gw sendiri

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("script connected");
+const dropdownButton = document.querySelector(".dropdown-trigger");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+dropdownButton.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("active");
+});
